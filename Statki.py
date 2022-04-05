fd34f426de1dd079ddc27c33ffcc8595c5ff1ff3
StanislawOlech <103060453+StanislawOlech@users.noreply.github.com>
2022-04-05 21:17:53 +0200

    Add files via upload
    trzeba:
    -dodać warunki by nie dało się strzelać poza plansze 
    -założyć jakieś limity dla losowania planszy z własnymi ustawieniami 
    -dodać by bot strzelał
    -zmienić x z liczb na literki

---
+++ Statki.py
@@ -0,0 +1,267 @@
+import random
+import numpy
+
+def clearConsole():
+    print('\n' * 50)
+
+def twist(x, y, l, n):
+    tab=[]
+
+    if (x +  l > 1 and x +  l < n+1):
+         tab.append([l, 0])
+
+    if (x -  l > 1 and x -  l < n+1):
+         tab.append([-l, 0])
+
+    if (y + l > 1 and y + l < n+1):
+        tab.append([0, l])
+
+    if (y - l > 0 and y - l < n+1):
+        tab.append([0, -l])
+
+    if len(tab) == 0:
+        return (0)
+
+    return (random.choice(tab))
+#ustawianie dłuższych statków w konkretnym kierunku
+
+def check(x, y, tab, h = [0, 0]):
+    ini = tab[h[1]][h[0]]
+    tab[h[1]][h[0]] = '0'
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if tab[j][i] == '■':
+                return 0
+    tab[h[1]][h[0]] = ini
+    return 1
+#sprawdzanie czy dokoła (x, y) jest pole z statkiem
+
+def rand(n, ship):#n wymiar planszy, ship liczba statków o konkretnym wymiarze
+    tab = create(n+2)
+
+    for j in range (1, len(ship)+1): #kolejne wielkości statków
+        for i in range(0, ship[j-1]):  #liczba statków
+            x = random.randint(1, n)
+            y = random.randint(1, n)
+
+            dir = twist(x, y, j, n)
+
+            if dir == 0:
+                return (0)
+
+            tiles = check(x, y, tab)
+            if tiles == 0:
+                return (0)
+
+            if (dir[0] > 0):
+                for a in range(1, j):
+                    tiles = check(x + a, y, tab, [x + a - 1, y])
+                    if tiles == 0:
+                        return (0)
+                    tab[y][x+a] = '■'
+
+            if (dir[0] < 0):
+                for a in range(1, j):
+                    tiles = check(x - a, y, tab, [x - a + 1, y])
+                    if tiles == 0:
+                        return (0)
+                    tab[y][x - a] = '■'
+
+            if (dir[1] > 0):
+                for a in range(1, j):
+                    tiles = check(x, y + a, tab, [x, y + a - 1])
+                    if tiles == 0:
+                        return (0)
+                    tab[y+a][x] = '■'
+
+            if (dir[1] < 0):
+                for a in range(1, j):
+                    tiles = check(x, y - a, tab, [x, y - a + 1])
+                    if tiles == 0:
+                        return (0)
+                    tab[y-a][x] = '■'
+
+            tab[y][x] = '■'
+
+
+
+    return tab
+#tworzy planszę z losowym ułożeniem statków i sprawdza czy jest ok
+#jak jest zle to zwraca 0
+
+def create(n):
+    dict = {-1: '', 0: 'a', 1: 'b', 2: 'c', 3: 'd', 4: 'e', 5: 'f', 6: 'g', 7: 'h', 8: 'i', 9: 'j', 10: 'k', 11: 'l', 12: 'm', 13: 'n', 14: 'o', 15: 'p', 16: 'q', 17: 'r', 18: 's', 19: 't', 20: 'u', 21: 'v', 22: 'w', 23: 'x', 24: 'y', 25: 'z'}
+    tab = [[0] * n for l in range(n)]
+    for i in range(n):
+        for j in range(n):
+            tab[i][j] = '□'
+        tab[i][n-1]=' '
+        tab[i][0] = i
+    for j in range(n):
+        tab[0][j] = dict[j - 1]
+        tab[n-1][j] = ' '
+        tab[0][n - 1] = ' '
+
+    return (tab)
+#tworzy plansze z obramowaniem
+
+def draw(tab):
+    for x in tab:
+        print(x)
+#wypisuje plansze
+
+def sunk(x, y, tab):
+    if check(x, y, tab) == 1:
+
+        for i in range(x - 1, x + 2):
+            for j in range(y - 1, y + 2):
+                if tab[j][i] == '□':
+                    tab[j][i] = 'x'
+#zatapia (obrysowuje) daną kratkę
+
+def wreck(x, y, tab, h):
+    ini = tab[h[1]][h[0]]
+    tab[h[1]][h[0]] = '0'
+    a = 0
+    if check(x, y, tab) != 1:
+        tab[h[1]][h[0]] = ini
+        return (1)
+
+
+
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if tab[j][i] == '⛝':
+                a = a + wreck(i, j, tab, [i, j])
+
+
+    print(a)
+
+    if a != 0:
+        tab[h[1]][h[0]] = ini
+        return 1
+
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if tab[j][i] == '⛝':
+                sunk(i, j, tab)
+
+    tab[h[1]][h[0]] = ini
+    return a
+#sprawdza czy statk jest zatopiony
+
+def shoot(tab):
+    x = int(input("podaj x"))
+    y = int(input("podaj Y"))
+
+    if tab[y][x] == '■':
+        tab[y][x] = '⛝'
+        a = wreck(x, y, tab, [x, y])
+        if a == 0:
+            sunk(x, y, tab)
+
+    elif tab[y][x] == '□':
+        tab[y][x] = 'x'
+#pobiera kordynaty z klawiatury i strzela
+
+def player_view(oldtab):
+    tab = numpy.copy(oldtab)
+
+    #for rows in tab:
+    #    for cell in rows:
+    #        if cell == '■':
+    #            cell = '□' #idk dlaczego nie działa
+
+    for x in range(0, len(tab)):
+        for y in range(0, len(tab)):
+            if tab[y][x] == '■':
+                tab[y][x] = '□'
+
+    draw(tab)
+#wypisuje to co widzi gracz
+
+def player_board(size, ship):
+    tab = create(size+2)
+    return (tab)
+#generuje plansze gracza
+
+def settings(pack):
+    print("modyfikacja liczby jedynek wcisnij 1")
+    print("modyfikacja  liczby dwójek 2")
+    print("modyfikacja liczby trójek 3")
+    print("modyfikacja liczby czwórek 4")
+    print("własne wielkosci statków wcisnij 5")
+    print("modyfikacja rozmiaru planszy wcisnij 6")
+    print("powrót 7")
+    num = int(input())
+
+    if num > 0 and num < 6:
+        pack[0] = ships_change(pack[0], num)
+
+    elif num == 6:
+        print("docelowa wartość")
+        pack[1] = int(input())
+#mapa ustawień (zmienia mapę lub odsyła do zmiany liczby statków
+
+def ships_change(ship, num):
+    new = numpy.zeros(num, dtype = int)
+
+    if num >= len(ship)+1:
+
+        for x in range(0, len(ship)):
+            new[x] = ship[x]
+
+        print("liczba statków o wielkości", num, 0)
+    else:
+        new = ship
+        print("liczba statków o wielkości", num, ship[num-1])
+    print("docelowa wartość:")
+
+    new[num - 1] = int(input())
+    clearConsole()
+    return (new)
+#zmiany liczby statków
+
+def welcome(pack):
+    print("Losowa plansza wcisnij 1", '\n', "Własna plansza wcisnij 2", '\n',"Ustawienia wcisnij 3")
+    num = int(input())
+
+    if num == 3:
+        settings(pack)
+        tab = welcome(pack)
+
+    elif num == 1:
+        tab = rand(pack[1], pack[0])
+        while tab == 0:
+            tab = rand(pack[1], pack[0])
+
+    elif num == 2:
+        tab = player_board(pack[1], pack[0])
+    return tab
+#witam
+
+if __name__ == '__main__':
+    ship = [4, 3, 2, 1]
+    size = 10 #rozmiar
+    pack = [ship, size]
+
+    tab = welcome(pack)
+    draw(tab)
+
+    bot = rand(pack[1], pack[0])
+    while bot == 0:
+        bot = rand(pack[1], pack[0])
+
+    player_view(bot)
+
+    i = 0
+    while i != 10:
+        shoot(bot)
+        clearConsole()
+        player_view(bot)
+        i = i + 1
+
+    draw(bot)
+
+
+
